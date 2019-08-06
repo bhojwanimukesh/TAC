b6adc7623b9cb6b4b005f80bb2e292bda78a9075
Mukesh Bhojwani <bhojwanimukesh@gmail.com>
2019-08-06 17:01:27 +0530

    Added the contents to the project repository

---
+++ events.tac.local/Models/NavigationItem.cs
@@ -0,0 +1,15 @@
+﻿namespace events.tac.local.Models
+{
+    public class NavigationItem
+    {
+        public NavigationItem(string title, string url, bool active = false)
+        {
+            Title = title;
+            URL = url;
+            Active = active;
+        }
+        public string Title { get; set; }
+        public string URL { get; set; }
+        public bool Active { get; set; }
+    }
+}
---
+++ events.tac.local/Models/NavigationMenuItem.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace events.tac.local.Models
+{
+    public class NavigationMenuItem : NavigationItem
+    {
+        public NavigationMenuItem(string title, string url, IEnumerable<NavigationMenuItem> children) : base (title, url, false)
+        {
+            Children = children != null ? children : new List<NavigationMenuItem>();
+        }
+
+        public IEnumerable<NavigationMenuItem> Children { get; private set; }
+    }
+}
