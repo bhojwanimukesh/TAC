4ff4896b6364436c107a638b36cfd871ab2bf9e6
MB2 <bhojwanimukesh@gmail.com>
2019-08-08 20:20:13 +0530

    Corrective actions to get rid of unwanted stuff

--- events.tac.local/Models/IEventDetails.cs
+++ events.tac.local/Models/IEventDetails.cs
@@ -1,9 +1,6 @@
 ﻿using Glass.Mapper.Sc.Configuration.Attributes;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace events.tac.local.Models
 {
