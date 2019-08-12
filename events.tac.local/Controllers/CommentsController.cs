7f5414c79521e83101311d0230c19f82881a3a5c
MB1 <bhojwanimukesh@gmail.com>
2019-08-12 11:59:21 +0530

    Removed all other glass mapper package references and added only Glass.Mapper.Sc.91.Core and Glass.Mapper.Sc.91.Mvc. Through TDS generated the code with header and item tt files. Tested the retrieval of RelatedEvents through Glass Mapper code generated classes.

--- events.tac.local/Controllers/CommentsController.cs
+++ events.tac.local/Controllers/CommentsController.cs
@@ -1,4 +1,5 @@
-﻿using events.tac.local.Models;
+﻿using events.tac.local.events.tac.local.sitecore.templates.TAC.Events;
+using events.tac.local.Models;
 using Glass.Mapper.Sc.Web.Mvc;
 using System.Web.Mvc;
 
@@ -17,6 +18,8 @@ namespace events.tac.local.Controllers
         public ActionResult Index()
         {
             var comments = MvcContext.GetContextItem<IEventDetails>().Comments;
+            var relatedEvents = MvcContext.GetContextItem<IEvent_Details>().RelatedEvents;
+            ViewData["relatedEvents"] = relatedEvents;
             return View(comments);
         }
     }
