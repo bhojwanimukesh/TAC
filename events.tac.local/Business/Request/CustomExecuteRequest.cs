9cdec314a0880a6fd94d06631bd6fa618e4f820a
MB1 <bhojwanimukesh@gmail.com>
2019-08-14 18:54:39 +0530

    Exercise on showing the user friendly error page when page not found. Moved the custom pipeline to specific place.

---
+++ events.tac.local/Business/Request/CustomExecuteRequest.cs
@@ -0,0 +1,57 @@
+﻿using Sitecore;
+using Sitecore.Abstractions;
+using Sitecore.Configuration;
+using Sitecore.Web;
+using System.Web;
+
+namespace events.tac.local.Business.Request
+{
+    public class CustomExecuteRequest : global::Sitecore.Pipelines.HttpRequest.ExecuteRequest
+    {
+        private readonly BaseLinkManager _baseLinkManager;
+        public CustomExecuteRequest(BaseSiteManager baseSiteManager, BaseItemManager baseItemManager, BaseLinkManager baseLinkManager) : base(baseSiteManager, baseItemManager)
+        {
+            _baseLinkManager = baseLinkManager;
+        }
+
+        protected override void PerformRedirect(string url)
+        {
+            if (Context.Site == null || Context.Database == null || Context.Database.Name == "core")
+            {
+                LogInfo("Attempting to redirect url {0}, but no Context Site or DB defined (or core db redirect attempted)", url);
+                return;
+            }
+
+            // need to retrieve not found item to account for sites utilizing virtualFolder attribute
+            var notFoundItem = Context.Database.GetItem(Context.Site.StartPath + Settings.ItemNotFoundUrl);
+
+            if (notFoundItem == null)
+            {
+                LogInfo("No 404 item found on site: {0}", Context.Site.Name);
+                return;
+            }
+
+            var notFoundUrl = _baseLinkManager.GetItemUrl(notFoundItem);
+
+            if (string.IsNullOrWhiteSpace(notFoundUrl))
+            {
+                LogInfo("Found 404 item for site, but no URL returned: {0}", Context.Site.Name);
+                return;
+            }
+
+            Sitecore.Diagnostics.Log.Info("Redirecting to {0}", notFoundUrl);
+            if (Settings.RequestErrors.UseServerSideRedirect)
+            {
+                HttpContext.Current.Server.TransferRequest(notFoundUrl);
+            }
+            else
+                WebUtil.Redirect(notFoundUrl, false);
+        }
+
+        private void LogInfo(string message, params string[] args)
+        {
+            Sitecore.Diagnostics.Log.Info(string.Format(message, args), this);
+        }
+    }
+
+}
---
+++ events.tac.local/Business/Request/Set404StatusCode.cs
@@ -0,0 +1,33 @@
+﻿using Sitecore.Configuration;
+using Sitecore.Pipelines.HttpRequest;
+using System;
+using System.Net;
+using System.Web;
+
+namespace events.tac.local.Business.Request
+{
+    public class Set404StatusCode : HttpRequestBase
+    {
+        protected override void Execute(HttpRequestArgs args)
+        {
+            // retain 500 response if previously set
+            if (HttpContext.Current.Response.StatusCode >= 500 || args.Context.Request.RawUrl == "/")
+                return;
+
+            // return if request does not end with value set in ItemNotFoundUrl, i.e. successful page
+            if (!args.Context.Request.Url.LocalPath.EndsWith(Settings.ItemNotFoundUrl, StringComparison.InvariantCultureIgnoreCase))
+                return;
+
+            LogWarning("Page Not Found: " + args.Context.Request.RawUrl + ", current status: " + HttpContext.Current.Response.StatusCode);
+            HttpContext.Current.Response.TrySkipIisCustomErrors = true;
+            HttpContext.Current.Response.StatusCode = (int)HttpStatusCode.NotFound;
+            HttpContext.Current.Response.StatusDescription = "Page not found";
+        }
+
+        private void LogWarning(string message, params string[] args)
+        {
+            Sitecore.Diagnostics.Log.Warn(string.Format(message, args), this);
+        }
+    }
+
+}
